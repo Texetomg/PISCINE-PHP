bab6de4607782f9e05704d4fcf8ea6dcfc206760
Texetomg <texetomg@gmail.com>
2019-04-30 16:54:46 +0300

    fix

--- 01/ex07/rostring.php
+++ 01/ex07/rostring.php
@@ -3,9 +3,8 @@
     if ($argc > 1) {
         $string = trim(preg_replace("/\s+/ ", " ", $argv[1]));
         $string = explode(" ", $string);
-        $buff = $string[0];
+        array_push($string, $string[0]);
         unset($string[0]);
-        array_push($string, $buff);
-        print_r($string);
+        echo implode(" ", $string) . "\n";
     }
 ?>
