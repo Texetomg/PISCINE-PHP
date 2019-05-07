ec72161ea5a1b187b6bc840787622f6edd3e6337
Texetomg <texetomg@gmail.com>
2019-05-07 17:56:46 +0300

    12-15

---
+++ 05/ex14/ex14.sql
@@ -0,0 +1,4 @@
+SELECT `floor_number` AS `floor`, sum(`nb_seats`) AS `seats`
+FROM db_bfalmer.cinema
+GROUP BY `floor`
+ORDER BY `seats` DESC;
---
+++ 05/ex15/ex15.sql
@@ -0,0 +1,3 @@
+SELECT reverse(`phone_number`) AS `rebmunenohp`
+FROM db_bfalmer.distrib
+WHERE `phone_number` LIKE '05%';
