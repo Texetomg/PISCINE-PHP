55bdd3224a91076572045a4cde10bc1f59ffe2f0
Texetomg <texetomg@gmail.com>
2019-05-07 17:26:48 +0300

    fix 07, add 09-11

---
+++ 05/ex09/ex09.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(*) AS `nb_short-films` FROM db_bfalmer.film WHERE `duration` <= 42;
---
+++ 05/ex11/ex11.sql
@@ -0,0 +1,11 @@
+SELECT      UPPER(`last_name`) AS `NAME`,
+            db_bfalmer.user_card.first_name,
+            db_bfalmer.subscription.price
+FROM        db_bfalmer.user_card,
+            db_bfalmer.member,
+            db_bfalmer.subscription
+WHERE       db_bfalmer.user_card.id_user = db_bfalmer.member.id_user_card
+                AND member.id_sub = subscription.id_sub
+                AND subscription.price > 42
+ORDER BY    user_card.last_name ASC,
+            user_card.first_name ASC;
