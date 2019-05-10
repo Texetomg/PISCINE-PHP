a44afe3c865294f3616567669fe6866f99018e6a
Texetomg <texetomg@gmail.com>
2019-05-10 16:20:47 +0300

    ?

--- 06/ex00/Color.class.php
+++ 06/ex00/Color.class.php
@@ -24,7 +24,7 @@
                 echo $this->__toString() . 'destructed' . PHP_EOL;
         }
         public function __toString() {
-            return sprintf("Color (red: %3d, green: %3d, blue %3d)", $this->red, $this->green, $this->blue);
+            return sprintf("Color (red: %3d, green: %3d, blue: %3d)", $this->red, $this->green, $this->blue);
         }
         public static function doc() {
             return file_get_contents('Color.doc.txt');
